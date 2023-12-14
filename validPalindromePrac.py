7901b3a4a8e2f54e3267e9faefcc409ce59753f5
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2023-12-13 21:04:34 -0800

    first commit

---
+++ validPalindromePrac.py
@@ -0,0 +1,41 @@
+'''
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. 
+Alphanumeric characters include letters and numbers.
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+Example 1:
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+
+Example 2:
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+
+Example 3:
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+'''
+class Solution(object):
+    def isPalindromeExtraMemory(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        pass
+
+
+
+    def isPalindrome(self, s):
+        pass
+
+    def alphaNum(self, c):
+        return (ord('A') <= ord(c) <= ord('Z') or
+                ord('a') <= ord(c) <= ord('z') or
+                ord('0') <= ord(c) <= ord('9'))
+    
+S = Solution()
+print(S.isPalindrome("A man, a plan, a canal: Panama"))
